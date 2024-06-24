1642c1f626741444f6611af8e1fb57d29034ca66
HuyLearnProgram <huylearnunity2021@gmail.com>
2024-06-24 16:19:11 +0700

    Cap nhat SP va fix bug Phieu Form

--- QLVT/Program.cs
+++ QLVT/Program.cs
@@ -23,8 +23,8 @@ namespace QLVT
 
         //Chuỗi kết nối về server
         public static string conStr = "";
-        //public static string conPublisher = "Data Source=HUNG-PC;Initial Catalog=QLVT;User ID=HTKN;Password=123456;TrustServerCertificate=True";
-        public static string conPublisher = "Data Source=LAPTOP-IG8QK02F;Initial Catalog=QLVT;Integrated Security=True;User ID=HTKN;Password=123456";
+        public static string conPublisher = "Data Source=HUNG-PC;Initial Catalog=QLVT;User ID=HTKN;Password=123456;TrustServerCertificate=True";
+        //public static string conPublisher = "Data Source=LAPTOP-IG8QK02F;Initial Catalog=QLVT;Integrated Security=True;User ID=HTKN;Password=123456";
 
 
 
